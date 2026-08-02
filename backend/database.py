6777c5ee30658465aeb9af2ca655b7217d92b7be
Sabari2005S <sabarishnadar2005@gmail.com>
2026-08-02 19:19:04 +0530

    Change MySQL connection settings
    Updated database connection details for Railway service.

--- backend/database.py
+++ backend/database.py
@@ -1,21 +1,12 @@
 import asyncmy
 
-
 async def get_connection():
-    try:
-        connection = await asyncmy.connect(
-            host="localhost",
-            port=3306,
-            user="root",
-            password="root123",      # Change if your MySQL password is different
-            database="employee_db",
-            autocommit=True,
-        )
-
-        print("✅ MySQL Connected Successfully")
-
-        return connection
-
-    except Exception as e:
-        print("❌ Database Connection Error:", e)
-        raise
+    connection = await asyncmy.connect(
+        host="mysql.railway.internal",
+        port=3306,
+        user="root",
+        password="lmdFdWPrbkJmVODzETqxvHYgahwSrLuw",
+        database="railway",
+        autocommit=True
+    )
+    return connection
