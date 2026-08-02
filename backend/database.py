49c22dc869c3435ad82750f4f782697a468ee9dd
Sabari2005S <sabarishnadar2005@gmail.com>
2026-08-02 20:37:12 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,12 @@
-import os
 import asyncmy
 
 async def get_connection():
     connection = await asyncmy.connect(
-        host=os.getenv("mysql.railway.internal"),
-        port=int(os.getenv("3306")),
-        user=os.getenv("root"),
-        password=os.getenv("lmdFdWPrbkJmVODzETqxvHYgahwSrLuw"),
-        database=os.getenv("railway"),
-        autocommit=True,
+        host="altaria.proxy.rlwy.net",
+        port=11569,
+        user="root",
+        password="lmdFdWPrbkJmVODzETqxvHYgahwSrLuw",
+        database="railway",
+        autocommit=True
     )
     return connection
